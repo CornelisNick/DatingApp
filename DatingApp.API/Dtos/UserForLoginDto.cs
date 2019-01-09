8af044db10256551fb83371aa79626023c459eda
CornelisNick <cornelisnick@hotmail.com>
2019-01-09 08:51:47 +0100

    End of section 6

--- DatingApp.API/Dtos/UserForLoginDto.cs
+++ DatingApp.API/Dtos/UserForLoginDto.cs
@@ -3,6 +3,6 @@ namespace DatingApp.API.Dtos
     public class UserForLoginDto
     {
         public string Username { get; set; }
-        public string password { get; set; }
+        public string Password { get; set; }
     }
 }
